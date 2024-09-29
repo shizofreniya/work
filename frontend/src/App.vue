6af572800bdfbbdad31b06fabdd28e956191c49b
shizofreniya <shizofazya@gmail.com>
2024-09-29 03:48:40 +0300

    mega krutoi readme

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <div class="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+  <div>
+    <span class="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+      <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><!-- ... --></svg>
+    </span>
+  </div>
+  <h3 class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Пишет вверх ногами</h3>
+  <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+    Перо в невесомости можно использовать для письма в любой ориентации, включая перевернутую. Он работает даже в космосе.
+  </p>
+</div>
 </template>
 
 <script>
